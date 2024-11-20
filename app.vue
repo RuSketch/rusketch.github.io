7376306b7e52fa8469d13394fa7c8485de79cbef
horanchikk <horandev.service@gmail.com>
2024-11-21 03:58:04 +0700

    feat: Добавлена yandex метрика

--- app.vue
+++ app.vue
@@ -1,8 +1,12 @@
 <template>
-  <main class="w-screen h-screen bg-[#141621] flex items-center justify-center font-semibold text-xl text-white">
+  <main
+    class="w-screen h-screen bg-[#141621] flex items-center justify-center font-semibold text-xl text-white"
+  >
     Здесь будет лендинг проекта RuSketch
   </main>
-</template>
+
+  <YaMetrika />
+</template> 
 
 <script lang="ts" setup>
 // remove on server
@@ -10,8 +14,12 @@
 useHead({
   lang: "ru",
   meta: [
-    { name: 'keywords', content: 'RUSketch, 3D-модели, маркетплейс, геймдев, анимация, архитектурная визуализация, фрилансеры, VFX, дизайн, Sketchfab аналог' },
-    { name: 'author', content: 'Команда RUSketch' }
+    {
+      name: "keywords",
+      content:
+        "RUSketch, 3D-модели, маркетплейс, геймдев, анимация, архитектурная визуализация, фрилансеры, VFX, дизайн, Sketchfab аналог",
+    },
+    { name: "author", content: "Команда RUSketch" },
   ],
-})
+});
 </script>
