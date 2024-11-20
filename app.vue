1f8f8f31b67f0b06b2695dcd1fa4d37d56ac3a3c
horanchikk <horandev.service@gmail.com>
2024-11-21 04:19:16 +0700

    feat(favicon): добавлена иконка сайта

--- app.vue
+++ app.vue
@@ -12,15 +12,22 @@
 // remove on server
 // defineOgImageComponent('LandingOg')
 useHead({
-  lang: "ru",
+  lang: 'ru',
   meta: [
     {
-      name: "keywords",
+      name: 'keywords',
       content:
-        "RUSketch, 3D-модели, маркетплейс, геймдев, анимация, архитектурная визуализация, фрилансеры, VFX, дизайн, Sketchfab аналог",
+        'RUSketch, 3D-модели, маркетплейс, геймдев, анимация, архитектурная визуализация, фрилансеры, VFX, дизайн, Sketchfab аналог',
     },
-    { name: "author", content: "Команда RUSketch" },
-    { name: "yandex-verification", content: "5cf4cb6a490ba370" }
+    { name: 'author', content: 'Команда RUSketch' },
+    { name: 'yandex-verification', content: '5cf4cb6a490ba370' }
   ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
 });
 </script>
