c2a9a43ff819dd882a43f2303d89432c8c2a4404
horanchikk <horanchikk@kansk-tc.ru>
2024-11-21 19:24:06 +0700

    feat: Добавлен контент сайта; добавлен редирект на яндекс форму

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
+  <Form />
+
   <main
-    class="w-screen h-screen bg-[#141621] flex items-center justify-center font-semibold text-xl text-white"
+    class="relative w-full bg-[#141621] flex items-center justify-center font-semibold text-xl text-white"
   >
-    Здесь будет лендинг проекта RuSketch
+    <img class="w-full select-none pointer-events-none" src="/background.png" alt="background">
+    <NuxtLink class="absolute top-0 left-0" to="https://forms.yandex.ru/cloud/673f1cac505690d5be6baeb0/" target="_blank">
+      <img class="w-full select-none pointer-events-none show" src="/content.png" alt="content">
+    </NuxtLink>
   </main>
 </template> 
 
@@ -10,3 +15,17 @@
 // remove on server
 // defineOgImageComponent('LandingOg')
 </script>
+
+<style>
+@keyframes show {
+  0% {opacity: 0; transform: translateY(-5px);}
+  100% {opacity: 1; transform: translateY(0px);}
+}
+
+.show {
+  opacity: 0;
+  animation: show ease-in-out 300ms;
+  animation-delay: 100ms;
+  animation-fill-mode: forwards;
+}
+</style>
