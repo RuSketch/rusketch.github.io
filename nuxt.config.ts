51a2495621cd8c8fee975ca5c6e91af8cea513ca
horanchikk <horanchikk@kansk-tc.ru>
2024-11-23 16:48:18 +0700

    fix: определён favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,8 +29,8 @@ export default defineNuxtConfig({
       link: [
         {
           rel: 'icon',
-          type: 'image/png',
-          href: '/logo.png'
+          type: 'image/x-icon',
+          href: '/favicon.ico'
         }
       ],
       script: [{ innerHTML: '!function(e,t,c,r,n,a,s){e.ym=e.ym||function(){(e.ym.a=e.ym.a||[]).push(arguments)},e.ym.l=1*new Date;for(var i=0;i<document.scripts.length;i++)if(document.scripts[i].src===r)return;a=t.createElement(c),s=t.getElementsByTagName(c)[0],a.async=1,a.src=r,s.parentNode.insertBefore(a,s)}(window,document,"script","https://mc.webvisor.org/metrika/tag_ww.js"),ym(98995601,"init",{clickmap:!0,trackLinks:!0,accurateTrackBounce:!0,webvisor:!0,trackHash:!0})' }]
